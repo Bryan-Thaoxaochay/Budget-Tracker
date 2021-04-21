0eeda443f4f28e5c3c88eea92388a74662b08e87
LittleMagician <bthaoxaochay@gmail.com>
2021-04-21 17:44:16 -0500

    Adding changes for help

--- public/service-worker.js
+++ public/service-worker.js
@@ -40,24 +40,6 @@ self.addEventListener("install", function (evt) {
     self.skipWaiting();
 });
 
-// activate
-self.addEventListener("activate", function (evt) {
-    evt.waitUntil(
-        caches.keys().then(keyList => {
-            return Promise.all(
-                keyList.map(key => {
-                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-                        console.log("Removing old cache data", key);
-                        return caches.delete(key);
-                    }
-                })
-            );
-        })
-    );
-
-    self.clients.claim();
-});
-
 // fetch
 self.addEventListener("fetch", function (evt) {
     // cache successful requests to the API
@@ -90,4 +72,22 @@ self.addEventListener("fetch", function (evt) {
         })
     );
     
+});
+
+// activate
+self.addEventListener("activate", function (evt) {
+    evt.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log("Removing old cache data", key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+
+    self.clients.claim();
 });
